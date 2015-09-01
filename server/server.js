efc05f843cebc7de19e09af68344fd6c15043552
Andre Roussakoff <madrus@gmail.com>
2015-09-02 01:08:19 +0200

    API works in cached form without persisting to the file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
+/*jshint node:true*/
 var express = require('express');
+// var path = require("path");
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
-var path    = require("path");
+var routes;
 
-app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname + '/../app/index.html'), {});
-})
-.listen(7777);
+app.set('port', process.env.PORT || 3500);
 
-console.log("Node Express server running at Port 7777");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+var server = app.listen(app.get('port'), function () {
+	console.log('Server up: http://localhost:' + app.get('port'));
+});
+
+routes = require('./routes/index')(app);
+
